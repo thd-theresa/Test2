a776ae951647bb264d42b79a7c6ef6d73376c3b0
Kolbinger Maximilian <maximilian.kolbinger@henrichsen.de>
2025-12-28 14:35:08 +0100

    Update User.java

--- src/main/java/de/th_deg/wib25/student_attendance/entity/User.java
+++ src/main/java/de/th_deg/wib25/student_attendance/entity/User.java
@@ -1,5 +1,51 @@
 package de.th_deg.wib25.student_attendance.entity;
 
 public class User {
-    System.out.println("User Test, hallo");
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType. IDENTITY)
+        private Long id;
+
+    @Column(nullable = false, unique = true)
+        private String email;
+
+    @Column(nullable = false)
+        private String password;
+        private String firstName;
+        private String lastName;
+
+    @Column(nullable = false)
+        private String role; // "PROFESSOR" oder "STUDENT"
+
+
+    // Constructors
+    public User() {}
+
+
+    // Getters & Setters
+    public Long getId() {
+        return id; }
+    public void setId(Long id) {
+        this.id = id; }
+    public String getEmail() {
+        return email; }
+    public void setEmail(String email) {
+        this.email = email; }
+    public String getPassword() {
+        return password; }
+    public void setPassword(String password) {
+        this.password = password; }
+    public String getFirstName() {
+        return firstName; }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName; }
+    public String getLastName() {
+        return lastName; }
+    public void setLastName(String lastName) {
+        this.lastName = lastName; }
+    public String getRole() {
+        return role; }
+    public void setRole(String role) {
+        this.role = role; }
 }
